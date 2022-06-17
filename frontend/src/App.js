cf38fbc7c803557286adf64fdd0f04f5fbe472ad
Ngan Tran <trann0709@gmail.com>
2022-06-17 15:04:16 -0500

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,24 @@ import Header from './components/Header';
 import Search from './components/Search';
 import { useState } from 'react';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
 
 function App() {
   const [word, setWord] = useState('');
+
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(word)
-  };
+    console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
 
   return (
     <div className="App">
